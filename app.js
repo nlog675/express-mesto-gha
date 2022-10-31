d6db8a86549ac5cefba6ae1dfc567061cc73fe7a
nlog <nlog675@gmail.com>
2022-10-31 16:13:49 +0300

    ghtests

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const { loginValidation, registerValidation } = require('./middlewares/validation');
-const { NotFoundError } = require('./utils/NotFoundError');
+const NotFoundError = require('./utils/NotFoundError');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
@@ -19,10 +19,10 @@ app.post('/signin', loginValidation, login);
 app.post('/signup', registerValidation, createUser);
 app.use('/', userRouter);
 app.use('/', cardRouter);
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Такой страницы не существует'));
-});
 app.use(errors());
+app.use('*', () => {
+  throw new NotFoundError('Такой страницы не существует');
+});
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка сервера' : message });
